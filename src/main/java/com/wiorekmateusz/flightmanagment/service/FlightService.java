daacecb76c6a60fbcfadbd13b46ab9f73f73424b
MateuszWiorek <mateusz.wiorek@onet.pl>
2019-06-03 09:52:41 +0200

    Added FlightService and implementation (not ended)

--- src/main/java/com/wiorekmateusz/flightmanagment/service/FlightService.java
+++ src/main/java/com/wiorekmateusz/flightmanagment/service/FlightService.java
@@ -1,4 +1,14 @@
 package com.wiorekmateusz.flightmanagment.service;
 
+import com.wiorekmateusz.flightmanagment.domain.Flight;
+import com.wiorekmateusz.flightmanagment.domain.Passenger;
+import com.wiorekmateusz.flightmanagment.domain.Plane;
+
+import java.util.List;
+
 public interface FlightService {
+    List<Flight> getAllFlights();
+    Flight getFlightById(int id);
+    void deleteFligght(int id);
+    void makeReservation( Plane plane, int places);
 }
---
+++ src/main/java/com/wiorekmateusz/flightmanagment/service/FlightServiceImpl.java
@@ -0,0 +1,51 @@
+package com.wiorekmateusz.flightmanagment.service;
+
+import com.wiorekmateusz.flightmanagment.domain.Flight;
+import com.wiorekmateusz.flightmanagment.domain.Passenger;
+import com.wiorekmateusz.flightmanagment.domain.Plane;
+import com.wiorekmateusz.flightmanagment.repository.FlightRepository;
+import com.wiorekmateusz.flightmanagment.repository.PassengerRepository;
+import com.wiorekmateusz.flightmanagment.repository.PlaneRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service("flightService")
+public class FlightServiceImpl implements FlightService {
+    FlightRepository flightRepository;
+    PassengerRepository passengerRepository;
+    PlaneRepository planeRepository;
+    @Override
+    public List<Flight> getAllFlights() {
+        return flightRepository.findAll();
+    }
+
+    @Override
+    public Flight getFlightById(int id) {
+        return flightRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteFligght(int id) {
+        Optional<Flight> flight= flightRepository.findById(id);
+        if(checkFlight(flight, id)){
+            flightRepository.deleteById(id);
+        }
+
+
+    }
+
+    @Override
+    public void makeReservation( Plane plane, int places) {
+        if (plane.getReservedPlaces()<plane.getSize()-places){
+
+        }
+    }
+
+    private boolean checkFlight(Optional<Flight> c, Integer id) {
+        return id != null && c.isPresent();
+    }
+}
